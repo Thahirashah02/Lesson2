2152775f200e070dd9822b887fe5097617a5df5f
Thairashah02 <thahirashahcodingal@gmail.com>
2024-11-10 09:03:02 +0300

    initialcommit

---
+++ activity1.py
@@ -0,0 +1,12 @@
+#print a sentence
+print("Welcome to Codingal!")
+
+#print a number
+print(5)
+
+#print combination of numbers and charecters
+print("hello",7)
+
+#print in new line
+print("hello \n", 7)
+print("hello \n python")
---
+++ activity2.py
@@ -0,0 +1,13 @@
+x=5
+y="john"
+print(x)
+print(y)
+
+
+name="zayed is a good boy"
+print(name)
+
+name= "Thahira is zayed's teacher"
+print(name)
+
+colour=input("Enter a colour:")
